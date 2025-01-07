08a7b686207b48a866f7bab4b3892510ae889216
SAI <sai@SAIs-MacBook-Air.local>
2025-01-07 22:30:15 +0530

    added semicolon and pushed into develop branch

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.listen(1234, function(){
     console.log('server is running on port 1234')
